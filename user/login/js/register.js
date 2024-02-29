0eada85d4433b252be2dbcef41da07df9c3de32f
Zlash01 <xhunterx2003@gmail.com>
2024-02-29 08:00:30 +0700

    update user login and register

---
+++ user/login/js/register.js
@@ -0,0 +1,54 @@
+document.querySelector(".register__btn").addEventListener("click", function () {
+  let flag = true;
+  const account = document.querySelector("#register__user").value.trim();
+  const password = document.querySelector("#register__pw").value.trim();
+  const password_again = document
+    .querySelector("#register__pw__again")
+    .value.trim();
+  console.log(account);
+  console.log(password);
+  console.log(password_again);
+
+  if (password !== password_again) {
+    flag = false;
+  } else if (!isValidPassword(password)) {
+    flag = false;
+  }
+
+  if (flag == true) {
+    console.log("Password is valid.");
+    alert("Password is valid.");
+  } else {
+    console.log(
+      "Password is invalid. It must have at least 1 uppercase letter, 1 lowercase letter, 1 number, and be 8 characters long."
+    );
+    alert(
+      "Password is invalid. It must have at least 1 uppercase letter, 1 lowercase letter, 1 number, and be 8 characters long."
+    );
+  }
+});
+
+function isValidPassword(password) {
+  // Password must be at least 8 characters long
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Password must contain at least one uppercase letter, one lowercase letter, and one number
+  var hasUpperCase = false;
+  var hasLowerCase = false;
+  var hasNumber = false;
+
+  for (var i = 0; i < password.length; i++) {
+    var char = password.charAt(i);
+    if (char >= "A" && char <= "Z") {
+      hasUpperCase = true;
+    } else if (char >= "a" && char <= "z") {
+      hasLowerCase = true;
+    } else if (!isNaN(parseInt(char))) {
+      hasNumber = true;
+    }
+  }
+
+  return hasUpperCase && hasLowerCase && hasNumber;
+}
---
+++ user/login/js/script.js
@@ -0,0 +1,45 @@
+document.querySelector(".login__btn").addEventListener("click", function () {
+  const account = document.querySelector("#login__user").value;
+  const password = document.querySelector("#login__pw").value;
+  console.log(account);
+  console.log(password);
+  if (isValidPassword(password)) {
+    // Password is valid
+    console.log("Password is valid.");
+    alert("Password is valid.");
+  } else {
+    // Password is invalid
+    console.log(
+      "Password is invalid. It must have at least 1 uppercase letter, 1 lowercase letter, 1 number, and be 8 characters long."
+    );
+    alert(
+      "Password is invalid. It must have at least 1 uppercase letter, 1 lowercase letter, 1 number, and be 8 characters long."
+    );
+  }
+});
+
+
+function isValidPassword(password) {
+  // Password must be at least 8 characters long
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Password must contain at least one uppercase letter, one lowercase letter, and one number
+  var hasUpperCase = false;
+  var hasLowerCase = false;
+  var hasNumber = false;
+
+  for (var i = 0; i < password.length; i++) {
+    var char = password.charAt(i);
+    if (char >= "A" && char <= "Z") {
+      hasUpperCase = true;
+    } else if (char >= "a" && char <= "z") {
+      hasLowerCase = true;
+    } else if (!isNaN(parseInt(char))) {
+      hasNumber = true;
+    }
+  }
+
+  return hasUpperCase && hasLowerCase && hasNumber;
+}
