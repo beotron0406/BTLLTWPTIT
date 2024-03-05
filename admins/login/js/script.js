3880b22cddb58facf5249e21e3f8054b133cec1a
DA-rr1ck <143769271+DA-rr1ck@users.noreply.github.com>
2024-03-05 15:46:50 +0700

    Script for admin login page

---
+++ admins/login/js/script.js
@@ -0,0 +1,48 @@
+document.querySelector(".login__btn").addEventListener("click", function () {
+    const account = document.querySelector("#login__user").value;
+    const password = document.querySelector("#login__pw").value;
+    console.log(account);
+    console.log(password);
+    if (isValidPassword(password)) {
+      // Password is valid
+      console.log("Password is valid.");
+      alert("Welcome!");
+  
+      window.location.href = "../../dashboard/html/index.html";
+  
+    } 
+    else if(account == "" || password == ""){
+        alert("Please enter both Username and Password!");
+    }
+    else {
+      // Password is invalid
+      console.log("Incorrect Password!");
+      alert("Incorrect Password!");
+    }
+  });
+  
+  function isValidPassword(password) {
+    // Password must be at least 8 characters long
+    if (password.length < 8) {
+      return false;
+    }
+  
+    // Password must contain at least one uppercase letter, one lowercase letter, and one number
+    var hasUpperCase = false;
+    var hasLowerCase = false;
+    var hasNumber = false;
+  
+    for (var i = 0; i < password.length; i++) {
+      var char = password.charAt(i);
+      if (char >= "A" && char <= "Z") {
+        hasUpperCase = true;
+      } else if (char >= "a" && char <= "z") {
+        hasLowerCase = true;
+      } else if (!isNaN(parseInt(char))) {
+        hasNumber = true;
+      }
+    }
+  
+    return hasUpperCase && hasLowerCase && hasNumber;
+  }
+  
